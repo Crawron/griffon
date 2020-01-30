c83f1f46c12697b30308c48bac8d6433301bf56c
Crawron <azertyaaron@gmail.com>
2020-01-30 00:33:28 -0500

    removed matcher helpers from test they're in their own file

--- src/Matchers/tests/Matcher.test.ts
+++ src/Matchers/tests/Matcher.test.ts
@@ -1,25 +1,4 @@
-import { Matcher, matcherHelpers, MatcherContext } from "../Matcher"
-
-const matchTest: Matcher = ctx => {
-  switch (ctx.message.content) {
-    case "match":
-      return ctx.match()
-    case "error":
-      return ctx.error("error message")
-    case "skip":
-      return ctx.skip()
-  }
-  throw "fuck"
-}
-
-const bot: any = "the"
-
-const getMockContext: (msg: string) => MatcherContext = (message: string) => ({
-  bot,
-  message: { content: message, author: { id: "111" } },
-  args: "",
-  ...matcherHelpers,
-})
+import { matchTest, getMockContext } from "./helpers"
 
 it("matches", () => {
   const result = matchTest(getMockContext("match"))
